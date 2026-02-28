f5e952540009a4e768f048d2a8ffa9b3abf73103
goofy <16620188468@163.com>
2026-02-28 18:55:34 +0800

    study1.py

---
+++ day1/study1.py
@@ -0,0 +1,32 @@
+import requests
+
+def test_get_post():
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+    r = requests.get(url)
+
+    assert r.status_code == 200
+    assert r.json()["id"] == 1
+
+def test_get_user():
+    url = "https://jsonplaceholder.typicode.com/users/1"
+    r = requests.get(url)
+
+    assert r.status_code == 200
+    assert r.json()["id"] == 1
+
+def test_create_post():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    data = {
+        "title": "test",
+        "body": "hello",
+        "userId":1
+    }
+    r = requests.post(url, json=data)
+
+    assert r.status_code == 201
+    assert r.json()["title"] == "test"
+
+if __name__ == '__main__':
+    test_create_post()
+    test_get_user()
+    test_get_post()
